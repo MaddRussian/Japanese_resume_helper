5e188748008481d3704f4cc629e57350d62891db
William Sebastian <okeefesebastian91@gmail.com>
2025-07-25 13:21:38 +0900

    set up the resumes controller

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,59 @@
+class ResumesController < ApplicationController
+  before_action :set_resume, only: %i[show edit update destroy]
+
+  def index
+    @resumes = Resume.all
+  end
+
+  def new
+    @resume = Resume.new
+    @resume.educations.build
+    @resume.experiences.build
+    @resume.skills.build
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    @resume.user = current_user
+
+    if @resume.save
+      redirect_to @resume, notice: 'Resume created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @resume.update(resume_params)
+      redirect_to @resume, notice: 'Resume updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @resume.destroy
+    redirect_to resumes_path, notice: 'Resume deleted.'
+  end
+
+  private
+
+  def set_resume
+    @resume = Resume.find(params[:id])
+  end
+
+  def resume_params
+    params.require(:resume).permit(
+      :title, :summary,
+      educations_attributes: [:id, :school_name, :degree, :field, :start_date, :end_date, :description, :_destroy],
+      experiences_attributes: [:id, :company_name, :title, :start_date, :end_date, :description, :_destroy],
+      skills_attributes: [:id, :name, :_destroy]
+    )
+  end
+end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,4 +4,8 @@ class Resume < ApplicationRecord
   has_many :experiences, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_one_attached :uploaded_file
+
+  accepts_nested_attributes_for :educations, allow_destroy: true
+  accepts_nested_attributes_for :experiences, allow_destroy: true
+  accepts_nested_attributes_for :skills, allow_destroy: true
 end
