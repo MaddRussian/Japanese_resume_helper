8f1367046634edadbaaa6b905be1b9714d627448
William Sebastian <okeefesebastian91@gmail.com>
2025-07-25 12:24:19 +0900

    added validations for resume model

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,7 @@
 class Resume < ApplicationRecord
   belongs_to :user
+  has_many :educations, dependent: :destroy
+  has_many :experiences, dependent: :destroy
+  has_many :skills, dependent: :destroy
   has_one_attached :uploaded_file
 end
